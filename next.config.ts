3ce4932e70f0a50fe9f965b37394e8fb7e93750a
Nyan Lin Aung <150507886+blaze-nyan@users.noreply.github.com>
2025-03-27 03:56:33 +0700

    fixed i18n configuration error

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  i18n: {
-    // Supported locales
-    locales: ["en", "th", "zh"],
-    // Default locale
-    defaultLocale: "en",
-    // Remove this line or set to false
-    // localeDetection: false,
-  },
+  // i18n: {
+  //   // Supported locales
+  //   locales: ["en", "th", "zh"],
+  //   // Default locale
+  //   defaultLocale: "en",
+  //   // Remove this line or set to false
+  //   // localeDetection: false,
+  // },
 };
 
 export default nextConfig;
