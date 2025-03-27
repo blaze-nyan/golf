60509e91dcac491f6a89d2aa0aba4a63fb4c67b4
Nyan Lin Aung <150507886+blaze-nyan@users.noreply.github.com>
2025-03-27 10:46:48 +0700

    ....

--- app/types/long.d.ts
+++ app/types/long.d.ts
@@ -1,22 +1,2 @@
-declare module "long" {
-  export default class Long {
-    constructor(low: number, high?: number, unsigned?: boolean);
-
-    static fromValue(val: string | number | Long): Long;
-    static fromString(str: string, radix?: number): Long;
-    static fromNumber(value: number, unsigned?: boolean): Long;
-    static fromBits(
-      lowBits: number,
-      highBits: number,
-      unsigned?: boolean
-    ): Long;
-
-    toNumber(): number;
-    toString(radix?: number): string;
-
-    // Add other methods as needed
-
-    static ZERO: Long;
-    static ONE: Long;
-  }
-}
+// This re-exports the existing @types/long definitions
+export * from "@types/long";
